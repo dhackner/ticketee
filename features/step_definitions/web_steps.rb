543044cec1486ad503052294015ec07f48065bf9
Dan Hackner <dan.hackner@gmail.com>
2012-10-03 17:38:12 -0700

    - Added missing assertion to "I should see" web step - Variable renaming/cleanup - Implemented "I should be on" web step

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,15 +1,28 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
 
-Given /^I am on (the homepage)$/ do |page_name|
+Given /^I am on (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
-When /^I add a new project called "(.*?)"$/ do |arg1|
+When /^I add a new project called "(.*?)"$/ do |project_name|
   click_link "New Project"
-  fill_in "Name", :with => arg1
+  fill_in "Name", :with => project_name
   click_button "Create Project"
 end
 
-Then /^I should see "(.*?)"$/ do |arg1|
-  has_content? arg1
+Then /^I should see "(.*?)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
+
+Then /^I should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
 end
