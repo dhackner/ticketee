c1935ec125e93311f41837f7995db3439f69cc66
Dan Hackner <dan.hackner@gmail.com>
2012-10-11 12:29:55 -0700

    Copied and updated code from Github to enable CSS 'within' selectors - was autogenerated in previous versions of rails.
    see: https://github.com/rails3book/ticketee/blob/master/features/support/selectors.rb

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,4 +1,22 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
+
+module WithinHelpers
+  def with_scope(locator)
+    locator ? within(*selector_for(locator)) { yield } : yield
+  end
+end
+World(WithinHelpers)
+
+# Single-line step scoper
+When /^(.*) within ([^:]+)$/ do |step_name, parent|
+  with_scope(parent) { step step_name }
+end
+
+# Multi-line step scoper
+When /^(.*) within ([^:]+):$/ do |step_name, parent, table_or_string|
+  with_scope(parent) { step "#{step_name}:", table_or_string }
+end
 
 Given /^I am on (.+)$/ do |page_name|
   visit path_to(page_name)
@@ -10,7 +28,7 @@ When /^I add a new project called "(.*?)"$/ do |project_name|
   click_button "Create Project"
 end
 
-Then /^I should( not)? see "(.*?)"$/ do |negated, text|
+Then /^I should( not)? see "([^"]*?)"$/ do |negated, text|
   assert_equal page.has_content?(text), negated.nil?
 end
 
